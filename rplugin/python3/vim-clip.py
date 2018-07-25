bc12b991ef8fabb532f4e2c233e51ef466099a63
George J. Danforth <georgejdanforth@gmail.com>
2018-07-24 22:55:15 -0400

    got visual selection writing out to a file

--- rplugin/python3/vim-clip.py
+++ rplugin/python3/vim-clip.py
@@ -7,6 +7,26 @@ class VimClip:
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @neovim.command('Clip', sync=True)
+    def mark(self, char):
+        return self.nvim.current.buffer.mark(char)
+
+    def get_visual_selection(self):
+
+        s_line, s_col = self.mark('<')
+        e_line, e_col = self.mark('>')
+
+        if all(map(lambda n: not n, [s_line, s_col, e_line, e_col])):
+            return
+
+        lines = self.nvim.eval('getline({}, {})'.format(s_line, e_line))
+        lines[0] = lines[0][s_col:]
+        lines[-1] = lines[-1][:e_col + 1]
+
+        return '\n'.join(lines)
+
+    @neovim.command('Clip', sync=True, range='')
     def clip(self, *args, **kwargs):
-        self.nvim.command('echo "SUHHHHHHHH DUDE"')
+        visual_selection = self.get_visual_selection()
+        if visual_selection is not None:
+            with open('test.out', 'w+') as f:
+                f.write(visual_selection)
